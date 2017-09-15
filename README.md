7915b329d4eaa0f0e0abb9e2254665259cc54d58
David Steinberg <david@resium.com>
2017-09-14 18:23:22 -0700

    Add bin, improve readme, add output to ignore

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Clinvar data import for the Variant Subscription Service
 
 Downloads Clinvar XML and text description of a directory `clinvar_data`.
+
+```
+npm install -g vss-dat-import
+vss-dat-import
+```
+
+## Todo
+
+* set the output directory
+* add progress indication
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var Dat = require('dat-node')
-
+console.log('Welcome to datvar!!!!')
+var directory = 'clinvar_data'
 // 1. Tell Dat where to download the files
-Dat('clinvar_data', {
+Dat(directory, {
   // 2. Tell Dat what link I want
   key: 'b1deb53c351b5f968d9f6742990ab3691da54000fe6afde2fb3024e07816fa51' // (a 64 character hash from above)
 }, function (err, dat) {
   if (err) throw err
-
+  console.log('Created a directory ' + directory +' and beginning transfer.');
+  console.log('')
+  console.log('If this is your first time downloading these data it may take a moment.')
   // 3. Join the network & download (files are automatically downloaded)
   dat.joinNetwork()
 })
